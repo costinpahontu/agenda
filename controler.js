3f819d61cbeac4c2fdfc17389a21912b43937652
costinpahontu <costin.pahontu@gmail.com>
2013-11-23 20:23:08 +0200

    am facut lista de  iteme

--- controler.js
+++ controler.js
@@ -1,3 +1,10 @@
-function Ctrl($scope) {
-  $scope.name = 'Whirled';
-}
+function AgendaController($scope) {
+        $scope.list = [];
+ 
+        $scope.add = function() {
+        $scope.list.push($scope.newload +$scope.space+ $scope.nextload);
+		$scope.space="  pentru ca ";
+        $scope.newload = "";
+		$scope.nextload = "";
+        }
+    }
