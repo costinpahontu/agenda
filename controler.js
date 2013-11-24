87521229f6d9d22c58594563c25d1bdde3943f52
costinpahontu <costin.pahontu@gmail.com>
2013-11-24 15:27:33 +0200

    butoanele de edit si delete si checbox

--- controler.js
+++ controler.js
@@ -1,10 +1,49 @@
-function AgendaController($scope) {
-        $scope.list = [];
- 
-        $scope.add = function() {
-        $scope.list.push($scope.newload +$scope.space+ $scope.nextload);
-		$scope.space="  pentru ca ";
-        $scope.newload = "";
-		$scope.nextload = "";
+var uid = 1;
+
+
+function ContactController($scope) {
+    
+    $scope.list = [
+        {id:0, 'first': 'primul lucru  |||||', 'second':'urmat de '}
+    ];
+    
+    $scope.saveList = function() {
+        
+        if($scope.newload.id == null) {
+             $scope.newload.id = uid++;
+             $scope.list.push($scope.newload);
+        } else {
+            
+             for(i in $scope.list) {
+                    if($scope.list[i].id == $scope.newload.id) {
+                        $scope.list[i] = $scope.newload;
+                    }
+             }                
+        }
+        $scope.newload = {};
+    }
+
+    
+    $scope.delete = function(id) {
+        
+        for(i in $scope.list) {
+            if($scope.list[i].id == id) {
+                $scope.list.splice(i,1);
+                $scope.newload = {};
+            }
+        }
+        
+    }
+    
+    
+    $scope.edit = function(id) {
+        for(i in $scope.list) {
+            if($scope.list[i].id == id) {
+                $scope.newload = angular.copy($scope.list[i]);
+            }
         }
     }
+	
+	
+	
+}
